5eca12c7243eac4ad1044d6be0f7c6a776c83b6a
Peita Lin <n6378056@gmail.com>
2019-06-02 23:23:40 +1000

    Display for dispute

--- src/resources/dispute.rs
+++ src/resources/dispute.rs
@@ -283,7 +283,7 @@ impl<'a> ListDisputes<'a> {
 }
 
 /// An enum representing the possible values of an `Dispute`'s `status` field.
-#[derive(Copy, Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
+#[derive(Display, Copy, Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum DisputeStatus {
     ChargeRefunded,
