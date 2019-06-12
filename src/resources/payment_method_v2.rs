a395111319698ba6d17ee1f227cd3ec1cf558ab6
Peita Lin <n6378056@gmail.com>
2019-06-12 18:54:25 +1000

    metadata as optional

--- src/resources/payment_method_v2.rs
+++ src/resources/payment_method_v2.rs
@@ -56,7 +56,7 @@ pub struct PaymentMethod {
     /// Set of key-value pairs that you can attach to an object.
     ///
     /// This can be useful for storing additional information about the object in a structured format.
-    pub metadata: Metadata,
+    pub metadata: Option<Metadata>,
 
     /// The type of the PaymentMethod.
     ///
