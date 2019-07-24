4f8f1a485baccf3a241b878ad3997c386228b70f
Peita Lin <n6378056@gmail.com>
2019-07-24 18:05:24 +1000

    Option<String> to Option<Vec<String>> for setup intents'

--- src/resources/setup_intent.rs
+++ src/resources/setup_intent.rs
@@ -46,7 +46,7 @@ pub struct SetupIntent {
     // pub payment_method_options: Option<Card>,
     pub payment_method_options: Option<SetupIntentCard>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub payment_method_types: Option<String>,
+    pub payment_method_types: Option<Vec<String>>,
     pub status: String,
     pub usage: String,
 }
